73d05088ec6c68c3ca8de721a574218dd1f69141
AlexD10S <alexfraga10@gmail.com>
2023-06-07 10:44:10 +0100

    cargo fmt

--- cli/src/query.rs
+++ cli/src/query.rs
@@ -1,4 +1,4 @@
-use crate::helper::{Chain, Api};
+use crate::helper::{Api, Chain};
 
 #[subxt::subxt(runtime_metadata_path = "metadata/polkadot_metadata.scale")]
 pub mod polkadot {}
@@ -9,32 +9,24 @@ pub mod kusama {}
 #[subxt::subxt(runtime_metadata_path = "metadata/rococo_metadata.scale")]
 pub mod rococo {}
 
-use polkadot::runtime_types::polkadot_parachain::primitives::Id;
 use kusama::runtime_types::polkadot_parachain::primitives::Id as KusamaId;
+use polkadot::runtime_types::polkadot_parachain::primitives::Id;
 use rococo::runtime_types::polkadot_parachain::primitives::Id as RococoId;
-
 
 // Checks if paraId holds any leases on the specified chain
 //
 pub async fn maybe_leases(
     api: Api,
     chain: Chain,
-    para_id: u32
+    para_id: u32,
 ) -> Result<bool, Box<dyn std::error::Error>> {
-    
     let query = match chain {
         Chain::DOT => polkadot::storage().slots().leases(Id(para_id)),
         Chain::KSM => kusama::storage().slots().leases(KusamaId(para_id)),
         Chain::ROC => rococo::storage().slots().leases(RococoId(para_id)),
     };
-    
-    match api
-        .storage()
-        .at_latest()
-        .await?
-        .fetch(&query)
-        .await?
-    {
+
+    match api.storage().at_latest().await?.fetch(&query).await? {
         Some(_) => Ok(true),
         _ => Ok(false),
     }

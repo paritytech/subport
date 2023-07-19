94073c3b70ed96b8ba8beb1f37fda32a10d14807
al3mart <11448715+al3mart@users.noreply.github.com>
2023-07-19 03:01:29 +0200

    refactor

--- cli/src/calls.rs
+++ cli/src/calls.rs
@@ -1,110 +1,84 @@
-use crate::helper::Api;
-use sp_core::Pair;
-use subxt::{tx::PairSigner, utils::AccountId32, PolkadotConfig};
+use subxt::{utils::AccountId32};
+use subxt::{OnlineClient, PolkadotConfig};
+use crate::utils::get_signer;
 
 // #[subxt::subxt(runtime_metadata_path = "metadata/rococo_metadata.scale")]
 // pub mod rococo {}
 #[subxt::subxt(runtime_metadata_path = "metadata/local_metadata.scale")]
 pub mod rococo {}
 
-const BLOCKS_SCHEDULED: u32 = 20;
-const REGISTER_DEPOSIT: u128 = 10_000;
-const FUNDS_MANAGER: u128 = 10_000_000_000_000; // 10 UNITS
-
 use rococo::runtime_types::polkadot_parachain::primitives::Id as RococoId;
 use rococo::runtime_types::polkadot_parachain::primitives::{HeadData, ValidationCode};
 use rococo::runtime_types::polkadot_runtime_common::assigned_slots::SlotLeasePeriodStart::Current;
 
-type Call = rococo::runtime_types::rococo_runtime::RuntimeCall;
+pub type Call = rococo::runtime_types::rococo_runtime::RuntimeCall;
 type RegistrarCall = rococo::runtime_types::polkadot_runtime_common::paras_registrar::pallet::Call;
 type AssignSlotsCall = rococo::runtime_types::polkadot_runtime_common::assigned_slots::pallet::Call;
 type SchedulerCall = rococo::runtime_types::pallet_scheduler::pallet::Call;
 type BalancesCall = rococo::runtime_types::pallet_balances::pallet::Call;
 type UtilityCall = rococo::runtime_types::pallet_utility::pallet::Call;
 
+const BLOCKS_SCHEDULED: u32 = 1205; // 2 epochs (600*2) + 5 blocks of margin
+const REGISTER_DEPOSIT: u128 = 10_000;
+const FUNDS: u128 = 10_000_000_000_000; // 10 UNITS
+
 //
-// Batch for different calls
+// Create batch call out of the given calls
 //
-pub async fn batch(
-    api: Api,
-    para_id: u32,
-    account_manager: AccountId32,
-    sovereign_account: AccountId32,
-    is_permanent_slot: bool,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let root = get_signer();
+pub fn create_batch_all_call(
+    calls: Vec<Call>,
+) -> Result<Call, Box<dyn std::error::Error>> {
 
-    let mut calls = Vec::<Call>::new();
-    let remove_lock_call = create_remove_lock_call(para_id)
-        .await
-        .expect("Error preparing the call for remove the lock for the parachain");
-    calls.push(remove_lock_call);
-    let fund_parachain_manager_call = create_force_transfer_call(account_manager)
-        .await
-        .expect("Error preparing the call to fund the parachain manager");
-    calls.push(fund_parachain_manager_call);
-    let fund_sovereign_account_call = create_force_transfer_call(sovereign_account)
-        .await
-        .expect("Error preparing the call to fund the sovereign account");
-    calls.push(fund_sovereign_account_call);
-    let schedule_assign_slot_call = create_schedule_assign_slots_call(para_id, is_permanent_slot)
-        .await
-        .expect("Error preparing the call to assign slots");
-    calls.push(schedule_assign_slot_call);
-    //let calls = [remove_lock_call, fund_parachain_manager_call, fund_sovereign_account_call, schedule_assign_slot_call];
-    //let batch_call = rococo::tx().utility().batch(calls);
+    let batch_call = Call::Utility(
+        UtilityCall::batch_all { calls: calls }
+    );
 
-    let batch_call = Call::Utility(UtilityCall::batch { calls: calls });
+    Ok(batch_call)
+}
 
-    let tx = rococo::tx().sudo().sudo(batch_call);
+//
+// Fund the parachain manager from the faucet address using sudo
+//
+pub fn create_force_transfer_call(
+    account_dest: AccountId32,
+) -> Result<Call, Box<dyn std::error::Error>> {
+    let faucet_address =
+        std::env::var("FAUCET_ADDRESS").expect("Error: No Faucer Address provided");
+    let account_source: AccountId32 = faucet_address.parse().unwrap();
 
-    api.tx()
-        .sign_and_submit_then_watch_default(&tx, &root)
-        .await?
-        .wait_for_finalized_success()
-        .await?
-        .has::<rococo::sudo::events::Sudid>()?;
-    Ok(())
+    let call = Call::Balances(BalancesCall::force_transfer {
+        source: account_source.into(),
+        dest: account_dest.into(),
+        value: FUNDS,
+    });
+
+    Ok(call)
 }
 
 //
 // Register the parachain with sudo
 //
-pub async fn force_register(
-    api: Api,
+pub fn create_force_register_call(
     para_id: u32,
-    account_manager: AccountId32,
+    manager_account: AccountId32,
     genesis_head: Vec<u8>,
     validation_code: Vec<u8>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let root = get_signer();
+) -> Result<Call, Box<dyn std::error::Error>> {
     let call = Call::Registrar(RegistrarCall::force_register {
-        who: account_manager,
+        who: manager_account,
         deposit: REGISTER_DEPOSIT,
         id: RococoId(para_id),
         genesis_head: HeadData(genesis_head),
         validation_code: ValidationCode(validation_code),
     });
 
-    let tx = rococo::tx().sudo().sudo(call);
-
-    api.tx()
-        .sign_and_submit_then_watch_default(&tx, &root)
-        .await?
-        .wait_for_finalized_success()
-        .await?
-        .has::<rococo::sudo::events::Sudid>()?;
-    Ok(())
+    Ok(call)
 }
 
 //
-// Private calls
+// Creates a call to schedule an assign perm / tmp slot call
 //
-
-//
-// Schedule the assign slots into a parachain with sudo
-//
-async fn create_schedule_assign_slots_call(
+pub fn create_scheduled_assign_slots_call(
     para_id: u32,
     is_permanent_slot: bool,
 ) -> Result<Call, Box<dyn std::error::Error>> {
@@ -130,37 +104,42 @@ async fn create_schedule_assign_slots_call(
 }
 
 //
-// Fund the parachain manager from the faucet address using sudo
+// Creates a call to remove the manager lock from the given para
 //
-async fn create_force_transfer_call(
-    account_dest: AccountId32,
+pub fn create_scheduled_remove_lock_call(
+    para_id: u32,
 ) -> Result<Call, Box<dyn std::error::Error>> {
-    let root = get_signer();
-    let faucet_address =
-        std::env::var("FAUCET_ADDRESS").expect("Error: No Faucer Address provided");
-    let account_source: AccountId32 = faucet_address.parse().unwrap();
-
-    let call = Call::Balances(BalancesCall::force_transfer {
-        source: account_source.into(),
-        dest: account_dest.into(),
-        value: FUNDS_MANAGER,
-    });
-    Ok(call)
-}
-
-//
-// Remove a manager lock from a para. This will allow the manager of a
-// previously locked para to deregister or swap a para without using governance.
-//
-async fn create_remove_lock_call(para_id: u32) -> Result<Call, Box<dyn std::error::Error>> {
+    
     let call = Call::Registrar(RegistrarCall::remove_lock {
         para: RococoId(para_id),
     });
-    Ok(call)
+
+    let scheduled_call = Call::Scheduler(SchedulerCall::schedule_after {
+        after: BLOCKS_SCHEDULED,
+        maybe_periodic: None,
+        priority: 0,
+        call: Box::new(call),
+    });
+
+    Ok(scheduled_call)
 }
 
-fn get_signer() -> PairSigner<PolkadotConfig, sp_core::sr25519::Pair> {
-    let mnemonic_phrase = std::env::var("SEED").expect("Error: No SEED provided");
-    let pair = sp_core::sr25519::Pair::from_string(&mnemonic_phrase, None).unwrap();
-    PairSigner::new(pair)
+// Sign and send the passed call and waits for
+pub async fn sign_and_send_call(
+    api: OnlineClient<PolkadotConfig>,
+    call: Call,
+) ->  Result<(), Box<dyn std::error::Error>> {
+
+    let utx = rococo::tx().sudo().sudo(call);
+
+    api.tx()
+        .sign_and_submit_then_watch_default(&utx, &get_signer())
+        .await?
+        .wait_for_in_block()
+        .await?
+        .wait_for_success()
+        .await?
+        .has::<rococo::sudo::events::Sudid>()?;
+
+    Ok(())
 }
--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -1,20 +1,39 @@
 use scale::Encode;
-use sp_core::crypto::{Ss58AddressFormatRegistry, Ss58Codec};
+use sp_core::{
+    crypto::{Ss58AddressFormatRegistry, Ss58Codec},
+    Pair,
+};
 use sp_runtime::MultiSigner;
+use subxt::{tx::PairSigner, utils::AccountId32, OnlineClient, PolkadotConfig};
 
+use crate::query::{maybe_leases, paras_registered};
+
+// Rococo types
 #[subxt::subxt(runtime_metadata_path = "metadata/local_metadata.scale")]
 pub mod rococo {}
 use rococo::runtime_types::polkadot_parachain::primitives::Id as RococoId;
 
+// Different chains we can connect to
+pub enum Chain {
+    DOT,
+    KSM,
+    ROC,
+}
+
+pub fn get_signer() -> PairSigner<PolkadotConfig, sp_core::sr25519::Pair> {
+    let mnemonic_phrase = std::env::var("SEED").expect("Error: No SEED provided");
+    let pair = sp_core::sr25519::Pair::from_string(&mnemonic_phrase, None).unwrap();
+    PairSigner::new(pair)
+}
+
 pub fn parse_validation_code(validation_code: String) -> Vec<u8> {
-    let mut parsed_validation_code = validation_code;
     // Remove "0x" from validation_code
     let parsed_validation_code = &validation_code[2..];
     // Decode the hex to bytes
     hex::decode(parsed_validation_code).expect("Decoding failed")
 }
 
-pub fn calculate_sovereign_account<Pair>(para_id: u32) -> Result<String, Box<dyn std::error::Error>>
+pub fn calculate_sovereign_account<Pair>(para_id: u32) -> Result<AccountId32, Box<dyn std::error::Error>>
 where
     Pair: sp_core::Pair,
     Pair::Public: Into<MultiSigner>,
@@ -28,6 +47,50 @@ where
     let public = array_bytes::hex2bytes(&public_str).expect("Failed to convert hex to bytes");
     let public_key = Pair::Public::try_from(&public)
         .map_err(|_| "Failed to construct public key from given hex")?;
+    let to_parse = public_key.to_ss58check_with_version(Ss58AddressFormatRegistry::SubstrateAccount.into());
+    Ok(to_parse.parse().unwrap())
+}
 
-    Ok(public_key.to_ss58check_with_version(Ss58AddressFormatRegistry::SubstrateAccount.into()))
+// Returns if the passed para_id already has a slot in Rococo
+pub async fn has_slot_in_rococo(
+    rococo_api: OnlineClient<PolkadotConfig>,
+    para_id: u32,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    let lease_rococo = maybe_leases(rococo_api, Chain::ROC, para_id).await;
+
+    if lease_rococo.unwrap() {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
+// Check if the parachain is registerd  in Rococo
+pub async fn is_registered(
+    rococo_api: OnlineClient<PolkadotConfig>,
+    para_id: u32,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    let is_registered_in_rococo = paras_registered(rococo_api, para_id).await;
+    if is_registered_in_rococo.unwrap() {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
+
+// Returns if the passed para_id is applicable for a permanent slot in Rococo
+pub async fn needs_perm_slot(
+    polkadot_api: OnlineClient<PolkadotConfig>,
+    kusama_api: OnlineClient<PolkadotConfig>,
+    para_id: u32,
+) -> Result<bool, Box<dyn std::error::Error>> {
+    let lease_polkadot = maybe_leases(polkadot_api, Chain::DOT, para_id).await;
+
+    let lease_kusama = maybe_leases(kusama_api, Chain::KSM, para_id).await;
+
+    if lease_kusama.unwrap() || lease_polkadot.unwrap() {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
 }
